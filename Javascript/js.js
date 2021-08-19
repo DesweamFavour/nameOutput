dcdefbdad60831237f8eba6a77061622d192c594
Favour Samuel A <favourdesweam135@gmail.com>
2021-08-19 18:08:57 +0100

    first commit

---
+++ Javascript/js.js
@@ -0,0 +1,7 @@
+myName = "Favour Samuel";
+tag = "This is an output from javascript";
+
+document.write(myName + '<br>');
+document.write(tag);
+// document.body.textContent = tag;
+document.body.style.fontSize = "3rem";
